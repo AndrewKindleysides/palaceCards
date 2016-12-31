996958953e951bc41c51d24e8f46b3c1870f36b3
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2016-12-31 11:15:33 +0000

    basic deal done

--- src/middleware/deal.js
+++ src/middleware/deal.js
@@ -2,39 +2,33 @@ var _ = require('underscore'),
     util = require('util');
 
 
-module.exports = function (deck, noOfPlayers) {
+module.exports = function(deck, noOfPlayers) {
     var state = {
         players: [],
         deck
     }
 
-    for (var i = 0; i <= noOfPlayers; i++) {
+    for (var i = 0; i < noOfPlayers; i++) {
         var player = {
             hand: [],
             table: []
         };
 
         for (var p = 0; p <= 9; p++) {
-            _.each(deck, deal);
-            deck = _.without(deck, _.findWhere(deck, ));
+            if (player.hand.length >= 0 && player.hand.length < 3) {
+                player.hand.push(_.first(deck));
+                deck = _.without(deck, _.findWhere(deck, _.first(deck)));
+            } else if (player.table.length >= 0 && player.table.length < 3) {
+                player.table.push(_.first(deck));
+                deck = _.without(deck, _.findWhere(deck, _.first(deck)));
+            } else if (player.table.length >= 3 && player.table.length < 6) {
+                var card = _.first(deck);
+                card.faceUp = true;
+                player.table.push(card);
+                deck = _.without(deck, _.findWhere(deck, _.first(deck)));
+            }
         }
-
         state.players.push(player);
     }
-
-    console.log(util.inspect(state, false, null))
-
-    function deal(card) {
-        player.hand.push(card);
-        // if (player.hand.lengh < 3) {
-        //     player.hand.push(card);
-        // } else if (player.table.length >= 0 || player.table.length < 4) {
-        //     player.table.push(card);
-        // } else {
-        //     card.faceUp = true;
-        //     player.table.push(card);
-        // }
-        // dealtCards.push(card);
-
-    }
+    console.log(state.players.length);
 }
